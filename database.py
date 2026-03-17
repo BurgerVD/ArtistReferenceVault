2638dc216621c277e9b74584d0397ea32dbec0b6
SVD <64867718+ShaheerVD@users.noreply.github.com>
2026-03-17 15:33:26 +0200

    -Added image tag table to db  and  its functions

--- database.py
+++ database.py
@@ -11,7 +11,7 @@ class DatabaseManager:
     def create_tables(self):
         # Create the vault table if it doesn't exist
         cursor = self.conn.cursor()
-        #Run only once to create the table 
+        #Folder table
         cursor.execute('''
             CREATE TABLE IF NOT EXISTS folders (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +19,29 @@ class DatabaseManager:
                 path TEXT UNIQUE NOT NULL
             )
         ''')
+        #Image tag table
+        #Unique tags
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tags(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                image_path TEXT NOT NULL,
+                tag TEXT NOT NULL,
+                UNIQUE(image_path,tag)
+            )           
+        ''')
+        
         self.conn.commit()    
+    
+    def add_tag(self,image_path,tag):
+        cursor= self.conn.cursor()
+        cursor.execute("INSERT OR IGNORE INTO tags (image_path,tag) VALUES (?,?)",(image_path,tag))
+        self.conn.commit()
+    
+    def get_tags_for_image(self,image_path):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT tag FROM tags WHERE image_path = ?",(image_path))
+        #return list of tuple [('hand',),('face',)]
+        return [row[0] for row in cursor.fetchall()]
     
     def add_folder(self,name,path):
         cursor = self.conn.cursor()
